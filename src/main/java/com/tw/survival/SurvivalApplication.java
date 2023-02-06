e03a2a6fa35d8f8964141153f37518d26f801675
DESKTOP-9SLSPUR\gundam <Allen@gmail.com>
2023-02-06 23:02:46 +0800

    更改Bean

---
+++ src/main/java/com/tw/survival/SurvivalApplication.java
@@ -0,0 +1,13 @@
+package com.tw.survival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivalApplication.class, args);
+	}
+
+}
