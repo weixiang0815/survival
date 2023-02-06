0a9b60b46426ae550998a0b3a2c6f8861c9eb77e
DESKTOP-9SLSPUR\gundam <Allen@gmail.com>
2023-02-06 23:02:46 +0800

    更改Bean

--- src/main/java/tw/survival/controller/Market/ProductController.java
+++ src/main/java/tw/survival/controller/Market/ProductController.java
@@ -6,48 +6,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import tw.survival.model.Market.ProductBean;
-import tw.survival.model.Market.ProductClassBean;
 import tw.survival.model.Market.ProductInventory;
 import tw.survival.model.Market.ProductInventoryRepository;
+import tw.survival.model.Market.ProductRepository;
 
 @Controller
 public class ProductController {
-	
+
 	@Autowired
-	private ProductInventoryRepository ProductInventoryDao;
-	
+	private ProductInventoryRepository productInventoryDao;
+
 	@Autowired
-	private ProductBean ProductBeanDao;
-	
+	private ProductRepository productBeanDao;
+
 	@ResponseBody
 	@PostMapping("ProductRepository/add")
 	public ProductInventory insertProductInventory() {
 		ProductInventory pdb = new ProductInventory();
 		pdb.setAmount(100);
 		pdb.setPrice(5000);
-		
-		ProductInventory response =ProductInventoryDao.save(pdb);
-		
+
+		ProductInventory response = productInventoryDao.save(pdb);
+
 		return response;
 	}
-	
+
 	@ResponseBody
 	@PostMapping("ProductRepository/addproduct")
 	public ProductBean insertProduct() {
 		ProductBean pdb1 = new ProductBean();
-		pdb1.setName("100");
-		pdb1.setContext("100");
-		pdb1.setLocal("100");
-		pdb1.setFk_product_class_id(null);
+		pdb1.setName("KJM700狙擊槍");
+		pdb1.setImg(new byte[100]);
+		pdb1.setContext("10發裝金屬彈匣");
+		pdb1.setPrice(8000);
+		pdb1.setRent_fee(1200);
+		pdb1.setProduct_class("狙擊槍");
 		
-		ProductClassBean pdb2 = new ProductClassBean();
-		
-		
-		ProductBean response1 =ProductBeanDao;
-		
-		return response1;
+//		pdb1.setProductClassBean(productClassDao.findById2(1));
+		productBeanDao.save(pdb1);
+
+		return pdb1;
 	}
-	
-	
-	
+
 }
+
