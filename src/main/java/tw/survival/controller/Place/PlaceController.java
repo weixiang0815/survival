9d0b51a462f6583369213c053c32d0cde06fbc5a
wayne <waynewei840412@gmail.com>
2023-02-12 16:01:35 +0800

    新增 場地修改刪除 但還有錯 待解決

--- src/main/java/tw/survival/controller/Place/PlaceController.java
+++ src/main/java/tw/survival/controller/Place/PlaceController.java
@@ -1,17 +1,22 @@
 package tw.survival.controller.Place;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.web.servlet.ModelAndView;
 
 import tw.survival.model.Place.PlaceBean;
 import tw.survival.service.Place.PlaceService;
@@ -53,6 +58,47 @@ public class PlaceController {
 			
 			return "失敗";
 		}
+	}
+	
+	@GetMapping("/place/all")
+	public ModelAndView getAllPlace(ModelAndView mav) {
+		List<PlaceBean> list = pService.getAllPlace();
+		mav.setViewName("Place/showAllPlace");
+		mav.getModel().put("list", list);
+		return mav;
+	}
+	
+	@GetMapping("/place/id")
+	public ResponseEntity<byte[]> getPhotoById(@RequestParam Integer id){
+		PlaceBean place1 = pService.getOnePlaceById(id);
+		byte[] placeFile = place1.getPlace_photo();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		
+		return new ResponseEntity<byte[]>(placeFile,headers,HttpStatus.OK);
+	}
+	
+	@GetMapping("/place/edit")
+	public String editPlacePage(@RequestParam("id")Integer id, Model model) {
+		PlaceBean p1 = pService.getOnePlaceById(id);
+		model.addAttribute("place", p1);
+		return "Place/editPlace";
+	}
+	
+	@PostMapping("/place/edit")
+	public  String sendEditPlace(@RequestParam("id") Integer id,@RequestParam("place_name") String place_name,
+			@RequestParam("place_address") String place_address, @RequestParam("place_photo") byte[] img ,
+			@RequestParam("place_fee") Integer place_fee,@RequestParam("place_capacity") Integer place_capacity) {
+		 PlaceService pService = new PlaceService();
+		 pService.updatePlaceById(id,place_name,place_address,img,place_fee,place_capacity);
+		 return "redirect:/place/all";
+		
+	}
+	
+	@DeleteMapping("/place/delete")
+	public String deletePlace(@RequestParam("id") Integer id) {
+		pService.deletePlaceById(id);
+		return "redirect:/place/all";
 	}
 
 }
