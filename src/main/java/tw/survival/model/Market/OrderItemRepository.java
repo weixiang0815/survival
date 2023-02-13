25f51cefbee3a02314aa5a0bd731aa70aa43f5c9
Allen <Test@gmail.com>
2023-02-13 18:11:34 +0800

    修正update

---
+++ src/main/java/tw/survival/model/Market/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package tw.survival.model.Market;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItemBean, Integer> {
+
+}
