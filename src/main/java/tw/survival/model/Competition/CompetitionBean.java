d767b632b02f76afa2c93cdf78da497b849e63eb
Henry Wang <henry940129@gmail.com>
2023-02-13 23:47:32 +0800

    完善活動實體

--- src/main/java/tw/survival/model/Competition/CompetitionBean.java
+++ src/main/java/tw/survival/model/Competition/CompetitionBean.java
@@ -98,9 +98,11 @@ public class CompetitionBean {
 	@JoinColumn(name = "fk_post_id")
 	private PostsBean post;
 
-//	@OneToOne(cascade = CascadeType.ALL)
-//	@JoinColumn(name = "fk_competition_prize_id")
-//	private CompetitionPrizeBean competitionPrizes;
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "competition")
+	private CompetitionPrizeBean competitionPrizes;
+
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "competition")
+	private CompetitionHistoryBean competitionHistory;
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "competition", cascade = CascadeType.ALL)
 	private Set<SignUpBean> signUps = new LinkedHashSet<SignUpBean>();
@@ -258,13 +260,21 @@ public class CompetitionBean {
 		this.post = post;
 	}
 
-//	public CompetitionPrizeBean getCompetitionPrizes() {
-//		return competitionPrizes;
-//	}
-//
-//	public void setCompetitionPrizes(CompetitionPrizeBean competitionPrizes) {
-//		this.competitionPrizes = competitionPrizes;
-//	}
+	public CompetitionPrizeBean getCompetitionPrizes() {
+		return competitionPrizes;
+	}
+
+	public void setCompetitionPrizes(CompetitionPrizeBean competitionPrizes) {
+		this.competitionPrizes = competitionPrizes;
+	}
+
+	public CompetitionHistoryBean getCompetitionHistory() {
+		return competitionHistory;
+	}
+
+	public void setCompetitionHistory(CompetitionHistoryBean competitionHistory) {
+		this.competitionHistory = competitionHistory;
+	}
 
 	public Set<SignUpBean> getSignUps() {
 		return signUps;
