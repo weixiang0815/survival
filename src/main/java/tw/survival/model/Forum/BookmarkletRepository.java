b627ba9cffdb99700ebfc3c56aa8131f6ba04b9e
王威翔（Henry Wang） <henry940129@gmail.com>
2023-02-17 10:26:34 +0800

    remove DDL setting

--- src/main/java/tw/survival/model/Forum/BookmarkletRepository.java
+++ src/main/java/tw/survival/model/Forum/BookmarkletRepository.java
@@ -3,10 +3,8 @@ package tw.survival.model.Forum;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 
 
