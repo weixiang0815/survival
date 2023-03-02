f5a61b7981408cb8ccfb45394d2f57631de2659c
Henry Wang <henry940129@gmail.com>
2023-03-02 23:13:37 +0800

    調整前台模板各嵌入檔案位置

--- src/main/java/tw/survival/controller/Front/FrontController.java
+++ src/main/java/tw/survival/controller/Front/FrontController.java
@@ -1,8 +1,14 @@
 package tw.survival.controller.Front;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
+
+	@GetMapping("/")
+	public String getHomepage() {
+		return "front/index";
+	}
 
 }
