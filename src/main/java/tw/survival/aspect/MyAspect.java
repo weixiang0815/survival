2c3776e997171952ee8fe573b9b6fa45a7c510d1
Henry Wang <henry940129@gmail.com>
2023-03-06 17:10:43 +0800

    刪除多餘的程式碼

--- src/main/java/tw/survival/aspect/MyAspect.java
+++ src/main/java/tw/survival/aspect/MyAspect.java
@@ -1,41 +1,28 @@
 package tw.survival.aspect;
 
-import java.lang.System.Logger;
-
-import javax.servlet.http.HttpSession;
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.AfterReturning;
-//import org.aspectj.lang.annotation.AfterThrowing;
-//import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-
-
-@SessionAttributes({"employee","player"})
+@SessionAttributes({ "employee", "player" })
 @Aspect
 @Controller
 public class MyAspect {
-		
-	
-	
+
 	@Pointcut("execution(* tw.survival.service..*(..))")
-    public void pointcut() {
-    }
-	
+	public void pointcut() {
+	}
+
 	@Before("pointcut()")
 	public void before(JoinPoint joinPoint) {
-        // 取得目前使用者的登入資訊，例如從 session 取得使用者物件
+		// 取得目前使用者的登入資訊，例如從 session 取得使用者物件
 		System.out.println("=====before advice starts=====");
 		System.out.println("Before method execution: " + joinPoint.getSignature().getName());
-        // 如果使用者未登入，拋出 UnauthorizedException
+		// 如果使用者未登入，拋出 UnauthorizedException
 		return;
-    }
-
+	}
 
 }
