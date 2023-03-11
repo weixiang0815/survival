08bc0f2a63fd01f476b8b7fab39d0b680696a958
Henry Wang <henry940129@gmail.com>
2023-03-11 23:32:41 +0800

    完成前台顯示所有活動相簿頁面與多條件搜尋功能

--- src/main/java/tw/survival/controller/front/R/Competition/CompetitionControllerFrontRAJAX.java
+++ src/main/java/tw/survival/controller/front/R/Competition/CompetitionControllerFrontRAJAX.java
@@ -1,6 +1,8 @@
 package tw.survival.controller.front.R.Competition;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import tw.survival.model.Competition.CompetitionBean;
+import tw.survival.model.Competition.CompetitionPictureBean;
+import tw.survival.model.Competition.CompetitionPictureDto;
 import tw.survival.model.Competition.CompetitionSearchCondititonsDto;
 import tw.survival.service.Competition.CompetitionPictureService;
 import tw.survival.service.Competition.CompetitionService;
@@ -33,6 +37,23 @@ public class CompetitionControllerFrontRAJAX {
 	@PostMapping("/multi-condition")
 	public List<CompetitionBean> multiCondition(@RequestBody CompetitionSearchCondititonsDto dto) {
 		return compService.multiconditionSearchFront(dto);
+	}
+	
+	@PostMapping("/photo/multi-condition")
+	public List<CompetitionPictureDto> multiConditionForPhoto(@RequestBody CompetitionSearchCondititonsDto dto) {
+		List<CompetitionBean> comps = compService.multiconditionSearchFront(dto);
+		List<CompetitionPictureDto> pictures = new ArrayList<>();
+		comps.forEach(comp -> {
+			Set<CompetitionPictureBean> pic = comp.getPictures();
+			if (pic != null && pic.size() != 0) {
+				CompetitionPictureDto picDto = new CompetitionPictureDto();
+				picDto.setCompId(comp.getId());
+				picDto.setCompName(comp.getMandarinName());
+				picDto.setPicSet(pic);
+				pictures.add(picDto);
+			}
+		});
+		return pictures;
 	}
 
 	@GetMapping(value = "/photo/{id}", produces = "image/jpeg")
